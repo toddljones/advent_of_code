f965bf7ea0f5269278a47a75443cc0e4da990907
Todd Jones <toddljones@users.noreply.github.com>
2018-12-04 02:49:01 -0600

    Dec02 puzzle complete
    * dec02 puzzle 1 working
    
    * dec02 puzzle 1 working
    
    * dec02 puzzle complete

---
+++ 2018/dec02/dec02.py
@@ -0,0 +1,316 @@
+#%%
+%cd /home/toddl/project/advent_of_code/2018/dec02/
+
+#%%
+input = """lsrivfotzgdxpkefaqmuiygchj
+lsrivfotzqdxpkeraqmewygchj
+lsrivfotzbdepkenarjuwygchj
+lsrivfotwbdxpkeoaqmunygchj
+lsrijfotzbdxpkenwqmuyygchj
+lsrivfotzbdxpkensqsuwygcdj
+lsrivfotubdxpkenzqmuwyschj
+lsrjvwotzbdxpkenaqjuwygchj
+lsrtvfotzbdxpkeaaqmuqygchj
+lscivzotzbdxpkenaqmuwygcnj
+ddrivfotzbdxpkenlqmuwygchj
+jsrivfvtzbdxpkenaqmufygchj
+csrivfotzxdxpkenaqguwygchj
+lprivfbtzbqxpkenaqmuwygchj
+lsrnvfotzbnxpkenaqmuwygchk
+lsiivfotzbdhpkencqmuwygchj
+lsrivfotzbyxpkenaqmzwygchc
+lsjivfotqbdxpvenaqmuwygchj
+lsrivfotzbdxpkencqmuwvgqhj
+lsrivfotzhdxpqenaqouwygchj
+lsrivfytzbnxpkenaqmuwygcsj
+llrivfotzbdxpkenaqmuwynchd
+lsuivfotzbdxpnenaqmuwygchk
+lsrtvootnbdxpkenaqmuwygchj
+ysrivfotzcdxpkenaqmuwhgchj
+lsrivfotxbdxpkefgqmuwygchj
+lsrmvfotzbaxpkenaqmuwygfhj
+lsrivfothbyxpkxnaqmuwygchj
+isrivfotzbdxpkenaqmkwygcht
+lhrivfotzbdxpkbnfqmuwygchj
+lsrivfotzbmxpkenaqmuwbgdhj
+lsrivvotzbdxcoenaqmuwygchj
+ssrwvfotzbdjpkenaqmuwygchj
+lsrivfotgbwxpkenaqmhwygchj
+lsrivfotzbdxpkenaqcuhygcyj
+lcdivfotzbdxpkenaqmuwxgchj
+ysbivfotzbdxpkenaqmuwkgchj
+lsrivfltzbdxxkenaqcuwygchj
+lsrivfotzbdxpkgnaqmunegchj
+fsqpvfotzbdxpkenaqmuwygchj
+lsriifotzbcxpkenaqmubygchj
+lsrivfotzjdxpkenaqmugygcjj
+tsrikfotzbdxpkeneqmuwygchj
+larivfotzbdxpkenaqmwwygcpj
+larivfotzbdxpkenaqmuayrchj
+lsravfotzbdxpkdoaqmuwygchj
+lsrivfotzbixpkenaqvtwygchj
+lsrixfotnbdxtkenaqmuwygchj
+lsrirfotzbdxpkeneqmuwygchv
+lsrivfofzedxpkenaqmswygchj
+lwrivfotzvdxpkenaqmuwygfhj
+lsrivfotzbdapkenaqmuqygehj
+lsrizfotgbdxpkenaqjuwygchj
+lsrivioxzbdxpkanaqmuwygchj
+lsrivfmtzbdxpkgnaqmuwcgchj
+lsrivfotzbdxpkeaaqmuofgchj
+lsrivfotvbdxpkenuqmuwygcht
+lsrivfothcdxpkenaqouwygchj
+lsgivfotzbdxpkenawmuwygchi
+lsrigfotzbdxpmonaqmuwygchj
+lsrivfotzbdxrkfnaqmuwygcha
+lsrivfopobdxpkenaqmuwygchv
+lsrejfotzbdxpkvnaqmuwygchj
+lsrivfotzbdxplenqqmuwygchz
+lsmivfotzbdppkebaqmuwygchj
+lsrivfotubdipkewaqmuwygchj
+lsrivfodnbhxpkenaqmuwygchj
+lsrivfotzbdxpkenaqmkwzgshj
+lsrixfotzbdxpkensqmuwygohj
+lsdivfotzbdxpkenaqmuwagcwj
+lsrimfotzbdxpkenaqmuwygcyu
+asnivfotzbdxpkenaqmbwygchj
+lseivfltfbdxpkenaqmuwygchj
+lsrivfbtzbdxpuenaqmuwyychj
+lsziafozzbdxpkenaqmuwygchj
+lsrivfotzbdxpkwnaomuwygchg
+ldrivfotzbdxpkeniqmuwygihj
+lsrivfotzbdxpkenaqhdwycchj
+lsrigfotzbdxphenaqmuwynchj
+lsripfotzbdxpgenaqmuwygchh
+lsrgvfoczbdxpkenaqmuwygihj
+lsribfotzbgxpkenaqhuwygchj
+lsrkvfotztdxpienaqmuwygchj
+lsrivfohzbdxpkenaqrxwygchj
+lsrivfoszbdxpkenavmuwygvhj
+lsrivfstzblxpkcnaqmuwygchj
+lzrivfotzbdxpkegaqmuwygchv
+lsrivtotzbdxpkenaqrumygchj
+lsgivfotzbdwpkenaqmuwhgchj
+lurivfotybdxpkenaqmuwygchg
+lsrivfogzbdxpkmnrqmuwygchj
+lsrivgotzbdxpkengqmuwygcwj
+lirivfbyzbdxpkenaqmuwygchj
+lwrivfotzbdxpkbjaqmuwygchj
+lsrivkotzbqxakenaqmuwygchj
+lxrivfotzbdxpkenaqmuwygshy
+lxxivfqtzbdxpkenaqmuwygchj
+lsrivfohzbdxpzenaqmuzygchj
+lsrivfotzndxekenaqmuwygcvj
+lsrdvfotzbdxpkenaqguiygchj
+lsrivfotzbdxpiehaqauwygchj
+atrivfotzbdxpkenaqmuwygchz
+lsrivfovzbdxpkenaqmuvygcwj
+lsrivfotzmdxpkennqmuwyxchj
+luvcvfotzbdxpkenaqmuwygchj
+lsriqfotzbdxpbenaqmuwygchg
+bsoivfotzudxpkenaqmuwygchj
+lsrivfotzbdxphenaqmhwxgchj
+lsrivfotzbdxpkenasmuwjgchw
+lsrivdotzboxpkenaqmuwykchj
+lsqivfotzbdxcdenaqmuwygchj
+lsrivfktzndypkenaqmuwygchj
+lwrivfotzbdxpkenaqmuolgchj
+lkrivfowzbdxpkenaqmbwygchj
+lsrivhotzbdxpkenaqyuwygvhj
+lsruvfotzbdxpkecaqmukygchj
+lsrivdotzbdsskenaqmuwygchj
+lsrivfotzbdxpkanaqmuwygcmc
+lsrgvfotubdxpkenrqmuwygchj
+psrivfotzbdxpkenaqmutygchd
+lsrivfitzbdxpkenagmiwygchj
+lsrivfotzbdxpkbnaqauwyrchj
+lsrivfotvbdxpjenaqmuwygchr
+lsrdvfoyzbjxpkenaqmuwygchj
+vsrivfothbdxpkenaqmuwyychj
+lyrivfotzpdxpkepaqmuwygchj
+lsgbqfotzbdxpkenaqmuwygchj
+lxrivfotzbdxpkenegmuwygchj
+lsrivfokzbdxpkenaqnuwyxchj
+lsrivfotubmxpkexaqmuwygchj
+lswivfvtzbdxpkenaqmuwygcgj
+lsrivfonzbdxpkenaqiuwygchc
+isrivlotzbdxpkenaqmuwygchf
+lsrilfozzbdxpkenaqmuwygcvj
+wsrivfotzbdxpkepaqmuwegchj
+lsrivfrtzbrxpkenaqquwygchj
+lsrivfotzbdxpkeqaqmuoygjhj
+lsrivfotzmdxpkenaqmuwyxchg
+lsrnvfotzbzxxkenaqmuwygchj
+ldrivfotzbdxpkenaqmlxygchj
+lsriofotzbdxpkenaqmwwmgchj
+lsrivfotzodxjkenaqmuwyglhj
+lsriviotzbdxpkegaqguwygchj
+lsrimfotzbdxpkanaqmuwygshj
+lwrzvfotzbdxpkenaqmuwygcfj
+lirivfotzbdxkkenvqmuwygchj
+lsrivfotlbdxpkeoaqmuwygahj
+lsxivfotzbdxpkenaqmuwwgchi
+lsrivfotzbdxpkenaqmukygzzj
+lsrtvfotzbdxskenaqmuwygcij
+lsgilfotzbdxpdenaqmuwygchj
+lsriyfotbbdxpkenaqmuwygchm
+lsrivfotabdxpkenaqmuwyghhs
+xsrizfotzbdxpkenaqmuwygczj
+lsrivfotybdxpkenaqquwygchx
+lsrzvfofzbdxpktnaqmuwygchj
+xsripfotzbdxpkenaqmqwygchj
+lsrivfotzbdspkenahmuwugchj
+lsmivfotzbdbpkenaqmuwygchy
+lsruvfotzbdxpkenaqqpwygchj
+lrmivfotzbdxpkenaqguwygchj
+lsnivfotzbdlpketaqmuwygchj
+lsrivfotzbdxjketaqjuwygchj
+lsrivxotzbdchkenaqmuwygchj
+lsrivootzbdxpkenaqmuwybmhj
+tsrivfdtzbdxpkenaqmuwpgchj
+lsrivmotzbdxpkxnaqmuwcgchj
+lsrivfotzadepkenaqmuwyichj
+dsrivfotrbdxpkenaqmuwtgchj
+lsrivfhtzbdxvkenoqmuwygchj
+lsrivfotzvdxbkenaqmbwygchj
+lsrxvcotzbdxpkenaqmuwygvhj
+lsrivfotzbdxykenaqmuwfgcha
+lsbivfotzbdxpkenaqmuwfvchj
+lfrivfotzbdcpkgnaqmuwygchj
+lsrivfotzbdxpwegdqmuwygchj
+lsrivfotyjdupkenaqmuwygchj
+gsrivfotzbdxpkenaemuwcgchj
+lsrivfodqbdxpkenaqmuwygchg
+lsrivfoczbdxpkenaqnuwwgchj
+lsrivpouzbhxpkenaqmuwygchj
+llbivuotzbdxpkenaqmuwygchj
+lfrivfofzbdxpkenaqmuwygchb
+lsrivfotzbdxpkenaumuwgghhj
+lsrivfotzbdxqaenazmuwygchj
+lsrivfotzbgxpkenkqmqwygchj
+lsrivfotzbdxpkensqiawygchj
+ljrijfotzbdxppenaqmuwygchj
+lsrivfoszbdxpkrnlqmuwygchj
+lsrijfotzbdxpcfnaqmuwygchj
+lsrivfotzbdopkebaqmuwytchj
+lsrivfonzbdxnkenalmuwygchj
+larivfouzbvxpkenaqmuwygchj
+lsryvfotzbdxpkensqmuwygyhj
+lsrivfztzbdxpkenaxmuwigchj
+lqkivfotzbdxpkenaqmuwygcht
+wsdivfotzbdxpkenbqmuwygchj
+lsrlvfotzadxpkencqmuwygchj
+lsrivfotoohxpkenaqmuwygchj
+lsrivfbuzbdfpkenaqmuwygchj
+psrivfotzbdxpkenawmuqygchj
+lsrivmotzbdxpkxnaqmuwcychj
+lsrivfotzvdgpkenaqmuwlgchj
+lcfivfstzbdxpkenaqmuwygchj
+lsrivfotzbddpkeeaqmuwygcij
+lsribfotzbdxpkenaqmuwugcyj
+lsrivfotzbdxakenaqmkwygyhj
+lsrivfotzbdxpkegaqmupyvchj
+lfrivfitzbdxpkenaqmuwygcrj
+lskivfotzbdxpkenaqmuwygwwj
+lsrivfotzddnpkenaqmuwfgchj
+lsrivfotzbdiukhnaqmuwygchj
+lfrivfotzbdxpkendqmuwygctj
+ljriqfotzvdxpkenaqmuwygchj
+lsrivfotzbdxpkeskqpuwygchj
+lsrivfotzbdxpkehaqmupygghj
+lsriyfotsbdxpkedaqmuwygchj
+lsrivfotzbdsjsenaqmuwygchj
+lsrivfotzbwxpienaqmuaygchj
+lsrivrotzbdxpkenaumuwygahj
+lsrivpotzfdxpkenaqmuwyjchj
+lsrivfomebdxpoenaqmuwygchj
+lswigfotpbdxpkenaqmuwygchj
+lsrivnotzbdxpkenaqmufrgchj
+lsrivfolbbdxpkenaqmuwygcqj
+lirivfotzbdxpknnaqeuwygchj
+lsrrvfxtzbdxpaenaqmuwygchj
+lspivfotzbdxpnsnaqmuwygchj
+lsrivfotzbyxpkenaqmawygcij
+lsrivfotzbfxpbenaqmuwyichj
+lsrivfotzbvxpjeyaqmuwygchj
+lyrihfotzbdxpknnaqmuwygchj
+uurivfotzbdxpkenaqmubygchj
+lsrivfotgbdxnkenaxmuwygchj
+lsriffotzbdxpkdnaqmuwygshj
+lsrisfotzbdxpkenaqzjwygchj
+lsrilfotzbdxpkenaqmuwygtgj
+lsrivfotzbdxzkenaqmuhmgchj
+hsrivfotzbdxprenaqauwygchj
+tsrevfotzbdupkenaqmuwygchj
+lsrizfotzbpxpkenaqmuwyrchj
+lsdivfotzbxxpkenaqmuhygchj
+lsrivfttzbyxpkenaqmuaygchj
+lsrivfotzodxpwenaqzuwygchj
+lsrivfotfbdxpkenaqvuwygyhj
+lsrivfotzzdxpknnaqmulygchj
+lsrjvvotzbdxpkenaqmuwjgchj
+lsrivuotzbdxpkeiaqxuwygchj
+lsrivfotzbdxpzenaqmmwygthj
+lsrivfotzbdxphenaqmuwyghvj"""
+input = input.splitlines()
+print(len(input))
+
+#%%
+twos = 0
+threes = 0
+twos_b = False
+threes_b = False
+# iterate over all boxids
+# get a count by boxid of characters
+# then increment the twos and threes counters
+# if any chars in the boxid fit the bill
+for boxid in input:
+    charmap = {}
+    twos_b = False
+    threes_b = False
+    # iterate through chars, count occurrences
+    for c in boxid:
+        charmap[c] = charmap.get(c, 0) + 1
+    # increment the twos, threes counters
+    for c, cnt in charmap.items():
+        if cnt == 2:
+            twos_b = True
+        elif cnt == 3:
+            threes_b = True
+    if twos_b:
+        twos += 1
+    if threes_b:
+        threes += 1
+print(twos, threes, twos*threes)
+
+#%%
+def match_count(list1, list2):
+    cnt = 0
+    for i in range(len(list1)):
+        if list1[i] == list2[i]:
+            cnt += 1
+    return cnt
+
+t = len(input)
+for i, boxid in enumerate(input):
+    print(f'{i} of {t}')
+    for j, boxid2 in enumerate(input):
+        if boxid == boxid2:
+            continue
+        compare.append([boxid, boxid2, match_count(boxid, boxid2)])
+print(len(compare))
+
+#%%
+import pandas as pd
+df = pd.DataFrame(compare)
+df.columns = ['boxid1', 'boxid2', 'match_count']
+df
+
+#%%
+print(df.sort_values('match_count', axis=0, ascending=False).head(2))
+"""
+                           boxid1                      boxid2  match_count
+50464  lsrivmotzbdxpkxnaqmuwcychj  lsrivmotzbdxpkxnaqmuwcgchj           25
+41783  lsrivmotzbdxpkxnaqmuwcgchj  lsrivmotzbdxpkxnaqmuwcychj           25
+"""
+
